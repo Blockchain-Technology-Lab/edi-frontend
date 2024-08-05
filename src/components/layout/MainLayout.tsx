db542eb9f75733e22adb1c3418a428ea7ac96c48
Leandro Dal Molin <leandro.dalmolin@iohk.io>
2024-08-05 08:57:11 +0100

    Adjust sticky sidebar so it doesn't overlap the footer on smaller devices

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -8,9 +8,9 @@ type Props = {
 
 export function MainLayout({ main, footer, sidebar }: Props) {
   return (
-    <div className="px-5 tablet:px-6">
-      <div className="flex flex-col gap-5 tablet:flex-row">
-        <div className="tablet:sticky tablet:top-0 tablet:min-w-60 tablet:w-1/5 tablet:h-screen">
+    <div className="flex flex-col min-h-screen px-5 tablet:px-6">
+      <div className="flex-1 flex flex-col gap-5 tablet:flex-row">
+        <div className="tablet:sticky tablet:top-0 tablet:min-w-60 tablet:w-1/5 tablet:h-full">
           {sidebar}
         </div>
         <main className="flex-1 tablet:pt-10">{main}</main>
