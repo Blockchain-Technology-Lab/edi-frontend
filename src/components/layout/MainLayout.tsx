161e920d2bc5ead1740505bbdf5b2b7d2e4b101d
Leandro Dal Molin <leandro.dalmolin@iohk.io>
2024-08-12 15:07:59 +0100

    Tweak sidebar semantics and spacing

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -10,9 +10,9 @@ export function MainLayout({ main, footer, sidebar }: Props) {
   return (
     <div className="flex flex-col min-h-screen px-5 tablet:px-6">
       <div className="flex-1 flex flex-col gap-5 tablet:flex-row">
-        <div className="tablet:sticky tablet:top-0 tablet:min-w-60 tablet:w-1/5 tablet:h-full">
+        <aside className="tablet:sticky tablet:top-0 tablet:min-w-60 tablet:w-1/5 tablet:h-full">
           {sidebar}
-        </div>
+        </aside>
         <main className="flex-1 tablet:pt-10">{main}</main>
       </div>
       {footer}
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -2,7 +2,7 @@ import { Logo, Menu, ThemeSwitcher } from "@/components"
 
 export function Sidebar() {
   return (
-    <div className="flex flex-col justify-between gap-5 py-5 h-full">
+    <div className="flex flex-col gap-8 py-5 tablet:pb-0">
       <div className="flex flex-col gap-14">
         <header className="mt-4">
           <Logo />
