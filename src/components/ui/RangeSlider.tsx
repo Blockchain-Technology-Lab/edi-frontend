801bd156df9d88c3eb5a1fc55f859639b35e6f4a
Leandro Dal Molin <leandro.dalmolin@iohk.io>
2024-08-12 16:54:23 +0100

    Tweak RangeSlider colours

--- src/components/ui/RangeSlider.tsx
+++ src/components/ui/RangeSlider.tsx
@@ -5,11 +5,11 @@ import { twJoin } from "tailwind-merge"
 export function RangeSlider(props: RadixSlider.SliderProps) {
   return (
     <RadixSlider.Root
-      className="relative flex items-center h-[16px]"
+      className="relative flex items-center h-[16px] mt-4"
       {...props}
     >
       <RadixSlider.Track className="relative bg-slate-200 dark:bg-white/20 flex-grow h-[2px]">
-        <RadixSlider.Range className="absolute bg-aqua-500 h-full" />
+        <RadixSlider.Range className="absolute bg-slate-800 dark:bg-slate-300 h-full" />
       </RadixSlider.Track>
       <Thumb />
       <Thumb />
@@ -21,8 +21,8 @@ function Thumb() {
   return (
     <RadixSlider.Thumb
       className={twJoin(
-        "block w-[16px] h-[16px] bg-white border border-slate-800",
-        "dark:bg-white dark:border-transparent focus:outline-none focus:bg-aqua-400"
+        "block w-[16px] h-[16px] bg-white border border-slate-800 cursor-pointer",
+        "dark:bg-slate-300 dark:border-transparent focus:outline-none focus:bg-slate-800 dark:focus:bg-white"
       )}
     />
   )
