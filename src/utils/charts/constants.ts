e0901f56a678a61cfb4949081013e67812ddd46d
Zeeshan Jan <hello@zeeshan.uk>
2024-09-12 12:49:19 +0100

    updated line chart colours

--- src/utils/charts/constants.ts
+++ src/utils/charts/constants.ts
@@ -65,12 +65,17 @@ export const SOFTWARE_LEDGER_NAMES = [
 export const SOFTWARE_COLOURS = [
   "rgba(255, 206, 86, 1)", // Yellow
   "rgba(54, 162, 235, 1)", // Blue
-  "rgba(75, 192, 192, 1)", // Green
+  "rgba(75, 192, 192, 1)", // Aqua
   "rgba(255, 99, 132, 1)", // Red
   "rgba(255, 159, 64, 1)", // Orange
   "rgba(153, 102, 255, 1)", // Purple
   "rgba(117, 117, 117, 1)", // Gray
-  "rgba(157, 102, 89, 1)"
+  "rgba(0, 204, 102, 1)", // Green
+  "rgba(255, 0, 204, 1)", // Pink
+  "rgba(0, 102, 204, 1)", // Dark Blue
+  "rgba(204, 102, 0, 1)", // Brown
+  "rgba(255, 102, 0, 1)", // Bright Orange
+  "rgba(0, 153, 76, 1)" // Forest Green
 ]
 
 export const SOFTWARE_DOUGHNUT_LEDGER_NAMES = [
