d4926ea88933f4c0d6fd773f656ecd2e036583e6
Zeeshan Jan <zeeshan.jan@ed.ac.uk>
2025-02-17 13:11:48 +0000

    added infographics

--- src/pages/changelog/index.tsx
+++ src/pages/changelog/index.tsx
@@ -1,7 +1,6 @@
-import React from "react"
 import { ChangelogTimeline } from "@/components"
 
-const ChangelogPage: React.FC = () => {
+const ChangelogPage = () => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Changelog</h1>
