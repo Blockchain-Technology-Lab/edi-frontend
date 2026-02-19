5f651dab3769f5c63a8486cba0b7db89850912c1
Zeeshan Jan <hello@zeeshan.uk>
2026-02-19 19:45:29 +0000

    fixed topcard height

--- src/components/ui/LayerTopCard.tsx
+++ src/components/ui/LayerTopCard.tsx
@@ -1,34 +1,36 @@
-import { GitHubButton } from "@/components";
-import { useNavigate } from "@tanstack/react-router";
+import { GitHubButton } from "@/components"
+import { useNavigate } from "@tanstack/react-router"
 
 type LayerTopCardProps = {
-  title: string;
-  description: React.ReactNode;
-  imageSrc: string;
-  methodologyPath?: string;
-  githubUrl: string;
-};
+  title: string
+  description: React.ReactNode
+  imageSrc: string
+  methodologyPath?: string
+  githubUrl: string
+}
 
 export function LayerTopCard({
   title,
   description,
   imageSrc,
   methodologyPath,
-  githubUrl,
+  githubUrl
 }: LayerTopCardProps) {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   return (
-    <div className="card lg:card-side bg-base-200 shadow-lg border border-base-300 h-full rounded-box">
-      <figure className="w-full lg:w-1/2 max-h-60">
+    <div className="card lg:card-side bg-base-200 shadow-lg border border-base-300 rounded-box h-full flex flex-col">
+      <figure className="w-full lg:w-1/3 max-w-xs">
         <img
           src={imageSrc}
           alt={title}
-          className="object-cover w-full h-full"
+          className="object-cover w-full h-auto"
         />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title text-2xl">{title}</h2>
-        <p>{description}</p>
+      <div className="card-body flex flex-col justify-between">
+        <div>
+          <h2 className="card-title text-2xl">{title}</h2>
+          <p>{description}</p>
+        </div>
         <div className="card-actions justify-end gap-2">
           <button
             className="btn btn-outline"
@@ -44,5 +46,5 @@ export function LayerTopCard({
         </div>
       </div>
     </div>
-  );
+  )
 }
