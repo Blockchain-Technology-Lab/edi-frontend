1441f5a9180448b428959e418695f9d96922cfc6
Zeeshan Jan <hello@zeeshan.uk>
2026-02-19 19:45:29 +0000

    fixed topcard height

--- src/components/ui/DistributionCard.tsx
+++ src/components/ui/DistributionCard.tsx
@@ -1,29 +1,29 @@
 type DistributionCardProps = {
-  title: string;
-  imageSrc: string;
-  onClick?: () => void;
-};
+  title: string
+  imageSrc: string
+  onClick?: () => void
+}
 
 export function DistributionCard({
   title,
   imageSrc,
-  onClick,
+  onClick
 }: DistributionCardProps) {
   return (
     <div
-      className="col-span-1 bg-base-200 hover:bg-base-300 cursor-pointer h-full rounded-box shadow-md flex flex-col hover:scale-[1.06] transition-transform"
+      className="col-span-1 bg-base-200 hover:bg-base-300 cursor-pointer rounded-box shadow-md flex flex-col hover:scale-[1.06] transition-transform h-full"
       onClick={onClick}
     >
       <div className="p-4">
         <h2 className="text-xl font-semibold">{title}</h2>
       </div>
-      <figure className="w-full h-24 sm:h-48 md:h-60 overflow-hidden max-h-60">
+      <figure className="w-full max-w-xs overflow-hidden">
         <img
           src={imageSrc}
           alt={title}
-          className="w-full h-full object-cover opacity-30 rounded-b-box hover:opacity-90"
+          className="w-full h-auto object-cover opacity-30 rounded-b-box hover:opacity-90"
         />
       </figure>
     </div>
-  );
+  )
 }
